90b124cc5b37eb60aed5c3b8815c862483f035eb
andrelfs01 <andre.luisfs01@gmail.com>
2019-11-10 00:02:20 -0300

    problema na variacao da velocidade

--- cto/agents.py
+++ cto/agents.py
@@ -1,96 +1,206 @@
 from mesa import Agent
 from mesa import Model
 from mesa.time import RandomActivation
+from random import uniform
+import math
+import time
+import numpy as np
 #from cto.random_walk import RandomWalker
 
 
 class TargetAgent(Agent):
-    '''
-    A sheep that walks around, reproduces (asexually) and gets eaten.
-
-    The init is the same as the RandomWalker.
-    '''
-
-    energy = None
     under_observation = {}
     pos = (1,1)
     unique_id = 't_'
+    destination = None
+    old_position = (0,0)
+    speed = 1
 
-    def __init__(self, unique_id, pos, model, moore, energy=None):
+    def __init__(self, unique_id, pos, model,target_speed, destination = None, old_position = None, ):
         super().__init__(unique_id, model)
-        self.energy = energy
         self.under_observation = {}
         self.unique_id = unique_id
-        self.model = model
-        self.moore = moore
+        self.destination = destination
+        self.old_position = old_position
+        self.speed = target_speed
 
     def step(self):
-        '''
-        A model step. Move, then eat grass and reproduce.
-        '''
-        self.random_move()
-        living = True
+        self.move()
+    
+    def move(self):
+        #print("dest: {}".format(self.destination))
+        if (self.destination is False or self.destination is None or self.pos == self.destination):
+            self.destination = self.new_destination()
+        self.trace_next_move()
+        
+        if ( self.old_position != self.pos):
+            print("pos: {} ".format(self.pos))
+            _x, _y = self.pos
+            self.model.grid.move_agent(self, self.correction_pos(_x, _y))
 
-    def random_move(self):
-        '''
-        Step one cell in any allowable direction.
-        '''
-        # Pick the next cell from the adjacent cells.
-        next_moves = self.model.grid.get_neighborhood(self.pos, self.moore, True)
-        next_move = self.random.choice(next_moves)
-        # Now move:
-        self.model.grid.move_agent(self, next_move)
+    def  correction_pos(self, continuos_x, continuos_y):
+        if continuos_x < 0:
+            continuos_x = 0
+        
+        if continuos_y < 0:
+            continuos_y = 0
 
+        if continuos_x >= self.model.grid.x_max:
+            continuos_x = self.model.grid.x_max - 0.1
+
+        if continuos_y >= self.model.grid.y_max:
+            continuos_y = self.model.grid.y_max - 0.1
+
+        return (continuos_x, continuos_y)
+
+    # Cada alvo calcula o próximo destino, a partir da posição atual (x,y) 
+    def new_destination(self):
+        x, y = self.pos
+        new_x = x + uniform(-25, 25)#Ele gera dois números aleatórios no intervalo [-25,+25]
+        new_y = y + uniform(-25, 25)#Ele gera dois números aleatórios no intervalo [-25,+25]
+#       desde que não caia fora do "cercado"
+        while (new_x > self.model.grid.width or new_x < 0):
+            new_x = x + uniform(-25, 25)
+ 
+        while (new_y > self.model.grid.height or new_y < 0):
+            new_y = y + uniform(-25, 25)
+
+        return (new_x, new_y)
+    
+    #funcao que define o movimento em um passo, de acordo com o destino do agente.
+    def trace_next_move(self):
+        #posicao atual
+        x_pos, y_pos = self.pos
+        #destino do agente
+        #print("det: {}".format(self.destination))
+        x_dest, y_dest = self.destination
+        
+        #se o X do destino é maior que o X atual, incrementa o X para o movimento desse passo
+        if (x_dest > x_pos):
+            x = x_pos + self.speed
+            #mesmo procedimento, agora aplicado ao Y
+            #se o Y do destino é maior que o Y atual, incrementa o Y para o movimetno nesse passo
+            if (y_dest > y_pos):
+                y = y_pos + self.speed
+            #se o Y do destino é menor que o Y atual, decrementa o Y para o movimento desse passo
+            elif (y_dest < y_pos):
+                y = y_pos - self.speed
+            #senão, o Y do destino é igual o Y atual, não altera o Y para o movimento desse passo
+            else:
+                y = y_pos
+        #se o X do destino é menor que o X atual, decrementa o X para o movimento desse passo
+        elif (x_dest < x_pos):
+            x = x_pos - self.speed
+            #mesmo procedimento, agora aplicado ao Y
+            if (y_dest > y_pos):
+                y = y_pos + self.speed
+            elif (y_dest < y_pos):
+                y = y_pos - self.speed
+            else:
+                y = y_pos
+        #senão, o X do destino é igual o X atual, não altera o X para o movimento desse passo
+        else:
+            x = x_pos
+            #mesmo procedimento, agora aplicado ao Y
+            if (y_dest > y_pos):
+                y = y_pos + self.speed
+            elif (y_dest < y_pos):
+                y = y_pos - self.speed
+            else:
+                y = y_pos
+
+        self.old_position = self.pos
+        self.pos = self.correction_pos( x, y)
+        return self.pos    
 
 class ObserverAgent(Agent):
-    '''
-    A wolf that walks around, reproduces (asexually) and eats sheep.
-    '''
 
-    m = Model()
-    m.schedule = RandomActivation(m)
-
-    energy = None
     under_observation = {}
     pos = (1,1)
     unique_id = 'o_'
-    model = m
+    multiplication_factor = 1
+    sensor_range = 5
 
-    def __init__(self, unique_id, pos, model, moore, energy=None):
+    def __init__(self, unique_id, pos, model, sensor_range, multiplication_factor):
         super().__init__(unique_id, model)
-        self.energy = energy
         self.pos = pos
         self.under_observation = {}
         self.unique_id = unique_id
-        self.model = model
-        self.moore = moore
+        self.sensor_range = sensor_range
+        self.multiplication_factor = multiplication_factor
+        self.destination = []
         
     def step(self):
-        self.random_move()
-        #self.energy -= 1
-
-        # If there are sheep present, eat one
-        x, y = self.pos
-        this_cell = self.model.grid.get_cell_list_contents([self.pos])
-        sheep = [obj for obj in this_cell if isinstance(obj, TargetAgent)]
-        if len(sheep) > 0:
-            sheep_to_eat = self.random.choice(sheep)
-            
-        if False:
-            self.model.grid._remove_agent(self.pos, self)
-           
+        if (self.model.observers_indications.size != 0):
+            self.destination = self.closer()
+            self.move()
         else:
-            if self.random.random() < 0.5:
-                # Create a new wolf cub
-                #self.energy /= 2
-                pass
+            self.move()
 
-    def random_move(self):
-        '''
-        Step one cell in any allowable direction.
-        '''
-        # Pick the next cell from the adjacent cells.
-        next_moves = self.model.grid.get_neighborhood(self.pos, self.moore, True)
-        next_move = self.random.choice(next_moves)
-        # Now move:
-        self.model.grid.move_agent(self, next_move)
+    def move(self):
+        new_position = self.trace_next_move()
+        self.model.grid.move_agent(self, new_position)
+        ## verify field of view
+        self.under_observation = self.check_fov()      
+
+
+    def check_fov(self):
+        in_fov = self.model.grid.get_neighbors(
+                    self.pos,
+                    int(self.sensor_range))
+        return in_fov
+    
+    #verify indication closer to observer  
+    def closer(self):
+        dist = self.model.grid.height + self.model.grid.width + 100
+        key = 0
+        closer = []
+        i = 0
+        for point_indication in self.model.observers_indications:
+            x1, y1 = self.pos
+            x2 = math.floor(point_indication[0])
+            y2 = math.floor(point_indication[1])
+            #d =  self.model.grid.get_distance(self.pos, point_indication)
+            d = math.sqrt( ((x1-x2)**2)+((y1-y2)**2))
+            if (d <= dist):
+                dist = d
+                key = i
+                closer = point_indication
+            i += 1
+        
+        #print (closer, self.pos, self.model.observers_indications)
+        #remove
+        self.model.observers_indications = np.delete(self.model.observers_indications, key, 0)
+        #return         
+        return (math.floor(closer[0]), math.floor(closer[1]))
+
+    def trace_next_move(self):
+        x_pos, y_pos = self.pos
+        x_dest, y_dest = self.destination
+        if (x_dest > x_pos):
+            x = x_pos + 1
+            if (y_dest > y_pos):
+                y = y_pos + 1
+            elif (y_dest < y_pos):
+                y = y_pos - 1
+            else:
+                y = y_pos
+        elif (x_dest < x_pos):
+            x = x_pos - 1
+            if (y_dest > y_pos):
+                y = y_pos + 1
+            elif (y_dest < y_pos):
+                y = y_pos - 1
+            else:
+                y = y_pos
+        else:
+            x = x_pos
+            if (y_dest > y_pos):
+                y = y_pos + 1
+            elif (y_dest < y_pos):
+                y = y_pos - 1
+            else:
+                y = y_pos
+        return (x ,y)
+
+    
--- cto/model.py
+++ cto/model.py
@@ -1,9 +1,9 @@
 from mesa import Model
-from mesa.space import MultiGrid
+from mesa.space import ContinuousSpace
 from mesa.time import RandomActivation
 from mesa.datacollection import DataCollector
 
-from cto.agents import TargetAgent, ObserverAgent
+from agents import TargetAgent, ObserverAgent
 
 from random import uniform
 import math
@@ -12,6 +12,8 @@ from sklearn.cluster import KMeans
 import numpy as np
 import time
 
+def verify_observation(model_cto):
+    return len(model_cto.targets_observed)
 
 class Modelo(Model):
     """
@@ -24,10 +26,10 @@ class Modelo(Model):
     target_speed=1.0
     active_prediction=False
     a=1
-    width = 20
-    height = 20
+    width = 150
+    height = 150
 
-    def __init__(self, N=1, O=1, sensorRange=5, target_speed=1.0, active_prediction=False, a=1, width = 20, height = 20):
+    def __init__(self, N=1, O=1, sensorRange=5, target_speed=1.0, active_prediction=False, a=1, width = 150, height = 150):
         super().__init__()
         #self.running = True
         self.num_agents = N
@@ -35,8 +37,8 @@ class Modelo(Model):
         self.target_speed = target_speed 
         self.multiplication_factor = 1.0/target_speed 
         self.sensor_range  = sensorRange # * self.multiplication_factor
-        self.targets_observed = {}
-        self.grid = MultiGrid(int(width), int(height), False)
+        self.targets_observed = set()
+        self.grid = ContinuousSpace(int(width), int(height), False)
         self.schedule = RandomActivation(self)
         self.observers_indications = []
         self.a = a
@@ -55,7 +57,7 @@ class Modelo(Model):
             # Add the agent to a random grid cell
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
-            a = TargetAgent("t_"+str(i), (x, y), self, False)
+            a = TargetAgent("t_"+str(i), (x, y), self, self.target_speed)
             self.schedule.add(a)
 
             
@@ -70,18 +72,21 @@ class Modelo(Model):
             point = self.observers_indications[i] 
             x = math.floor(point[0])
             y = math.floor(point[1])
-            b = ObserverAgent("o_"+str(i), (x, y), self, False)
+            b = ObserverAgent("o_"+str(i), (x, y), self, self.sensor_range, self.multiplication_factor)
             self.schedule.add(b)
-
             
             self.grid.place_agent(b, (x, y))
 
         self.datacollector = DataCollector(
-            agent_reporters={"Observation": "under_observation"})
+            model_reporters={"Observation": "mean_observation"})
 
 
     def step(self):
+        # if (self.schedule.get_agent_count > 5200):
+        #     pass
+
         if ((self.schedule.steps % 10) == 0):
+                print("step: "+str(self.schedule.steps))
                 # print ("running kmeans...")
                 if(not self.active_prediction):
                     # print("kmeans")
@@ -89,28 +94,40 @@ class Modelo(Model):
                 else:
                     self.observers_indications = self.kmeans_predicted_indications()
         self.schedule.step()
-        print(self.schedule.get_agent_count)
+        #print(self.schedule.get_agent_count)
         
-        for a in self.grid.coord_iter():
-            agente, x, y = a
-            if len(agente.under_observation) > 0:
-                self.targets_observed.union(agente.under_observation)
+        
+        self.targets_observed = set()
+        #for a in self.grid.coord_iter():
+        for a in self.grid._agent_to_index:
+            if len(a.under_observation) > 0:
+                #print("{} : under {}".format(agente.unique_id,agente.under_observation))
+                #print("self set {} ---- get {}".format(self.targets_observed,self.get_targets(agente.under_observation)))
+                self.targets_observed = self.targets_observed.union(self.get_targets(a.under_observation))
+                #print("observed {}".format(self.targets_observed))
 
         self.datacollector.collect(self)
+
+    def get_targets(self, set_observation):
+        t = set()
+        for a in set_observation:
+            if ('t_' in a.unique_id):
+                #print("entrou: {}".format(a.unique_id))
+                t.add(a.unique_id)
+        return t
 
     def kmeans_indications(self):
         x_list = []
         y_list = []
-        for a in self.grid.coord_iter():
-            agent, x, y = a
-            
-            if (len(agent) != 0):
-                for ag in agent:
-                    
-                    if ('t_' in ag.unique_id):
-                        print (ag.unique_id)
-                        y_list.append(y)
-                        x_list.append(x)
+                
+        for a in self.grid._agent_to_index:
+            #
+            # print (a)
+            if ('t_' in a.unique_id):
+                x, y = a.pos
+                #print (a.pos)
+                y_list.append(y)
+                x_list.append(x)
         Data = {'x': x_list,
                 'y': y_list }        
   
@@ -123,17 +140,15 @@ class Modelo(Model):
     def kmeans_predicted_indications(self):
         x_list = []
         y_list = []
-        for a in self.grid.coord_iter():
-            agent, x, y = a
-            
-
-            # print (agent)
+        
+        for a in self.grid._agent_to_index:
             # calcular a equacao
-            if (agent != None and 't_' in agent.unique_id):
+            if (a != None and 't_' in a.unique_id):
+                x, y = a.pos
                 old_x = x
                 old_y = y
-                if (agent.old_position != None):
-                    old_x, old_y = agent.old_position
+                if (a.old_position != None):
+                    old_x, old_y = a.old_position
                 x_predicted = x + self.a * (x - old_x)
                 y_predicted = y + self.a * (y - old_y)
                 if (x_predicted < 0):
